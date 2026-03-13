00e0e335aa2efd91b73ef485c85a63ba7784b2ed
git config --global user.name finezdevl-hue <finezdevl@gmail.com>
2026-03-13 13:26:09 +0530

    fix build

--- hotelbook/settings.py
+++ hotelbook/settings.py
@@ -12,13 +12,13 @@ DEBUG = os.environ.get("DEBUG") == "True"
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
+    'django.contrib.staticfiles',
     'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary',
     'crispy_forms',
     'crispy_bootstrap5',
